94785ae2b58f434a72332c8b68ff1ffe91c2e7f0
radar <juha@uit.no>
2025-12-11 17:10:41 +0100

    asd

--- mf_conf.py
+++ mf_conf.py
@@ -1,5 +1,5 @@
 import numpy as n
-import datetime
+#import datetime
 import time
 from datetime import datetime, timedelta, timezone
 
@@ -9,7 +9,7 @@ dc_offset=-0.25-0.25j
 
 def unix2date(x):
     # ATC bug-fix for python3
-    return datetime.datetime.utcfromtimestamp(float(x))
+    return datetime.utcfromtimestamp(float(x))
 
 def unix2dirname(x):
     return (unix2date(x).strftime('%Y-%m-%d'))
@@ -30,7 +30,7 @@ def fir_lowpass_hann(fc=20e3, fs=1000000, num_taps=100):
     h = h / n.sum(h)
     return h
 def unix2datestr(unix_seconds):
-    date_string = datetime.datetime.utcfromtimestamp(unix_seconds).isoformat() + "Z"
+    date_string = datetime.utcfromtimestamp(unix_seconds).isoformat() + "Z"
     return(date_string)
 
 
@@ -39,13 +39,13 @@ def get_prev_day_bounds():
     now = datetime.now(timezone.utc)
 
     # Previous day
-    yesterday = now.date() - timedelta(days=1)
+    yesterday = now.date() - timedelta(days=0)
     
     # Start of previous day (00:00:00 UTC)
     start = datetime(yesterday.year, yesterday.month, yesterday.day, tzinfo=timezone.utc)
     
     # End of previous day (23:59:59 UTC)
-    end = start + timedelta(days=1) - timedelta(seconds=1)
+    end = start + timedelta(days=1) #- timedelta(seconds=1)
     
     start_unix = int(start.timestamp())
     end_unix = int(end.timestamp())
